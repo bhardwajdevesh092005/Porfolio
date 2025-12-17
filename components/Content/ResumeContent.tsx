d1477465f88d6238a65419920f72933f2160e93a
bhardwajdevesh092005 <b23cs1014@iitj.ac.in>
2025-12-17 21:08:21 +0530

    Debugged the image and pdf paths

--- components/Content/ResumeContent.tsx
+++ components/Content/ResumeContent.tsx
@@ -10,7 +10,7 @@ const ResumeContent: React.FC = () => {
       gap: '1rem'
     }}>
       <iframe
-        src="/Resume_Dev.pdf"
+        src={`Porfolio/Resume_Dev.pdf`}
         style={{
           width: '100%',
           height: '100%',
@@ -20,7 +20,7 @@ const ResumeContent: React.FC = () => {
         title="Resume PDF"
       />
       <a 
-        href="/Resume_Dev.pdf" 
+        href={`Porfolio/Resume_Dev.pdf`} 
         download="Devesh_Bhardwaj_Resume.pdf"
         style={{
           display: 'inline-block',
